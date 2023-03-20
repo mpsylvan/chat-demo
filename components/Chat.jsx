6104ecf8413d5a10b5b21b7a15127f1d4ddb164e
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-03-20 14:08:05 -0400

    includes some comments

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -5,14 +5,17 @@ import {Bubble, GiftedChat} from 'react-native-gifted-chat';
 
 const Chat = ({route, navigation}) => {
 
+// puts a message array on state that will load in existing messages and accept new sent messages. 
 const [messages, setMessages] = useState([]);
 
 const {user, color} = route.params;
 
+// every time the component mounts load props data passed from Start.jsx into navigation bar display options.
 useEffect(()=>{
     navigation.setOptions({title: user, color: color})
 }, []);
 
+// every mount of Chat component, load in an array of static message objects onto state; the objects meet the configurations of a <GiftedChat/> component and it's props.
 useEffect(()=>{
   setMessages([
     {
@@ -24,30 +27,44 @@ useEffect(()=>{
         name : 'React Native',
         avatar: 'https://placeimg.com/140/140/any'
       },
+      renderUsernameOnMessage: true,
     },
     {
-      _id: 2, 
-      text: 'User is not online currently.',
+      _id: 2,
+      text: 'New Message incoming ...',
+      createdAt : new Date(),
+      user: {
+        _id: 2, 
+        name : 'React Native',
+        avatar: 'https://placeimg.com/140/140/any',
+        renderUsernameOnMessage: true,
+      },
+    },
+    {
+      _id: 3, 
+      text: `${user} has entered the chat.`,
       createdAt: new Date(),
       system: true,
     }
   ]);
 },[])
 
+// resets the 'messages' state to append the newest message on every SEND executed within GiftedChat component. 
 const onSend = (newMessages)=>{
   setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
 }
 
+// function that returns a customization object with props awareness which will be loaded as a callback on the renderBubble prop in the GiftedChat component. 
 const renderBubble = ( props )=>{
   return (
     <Bubble
       {... props}
       wrapperStyle = {{
         right: {
-          backgroundColor: 'black'
+          backgroundColor: color
         },
         left: {
-          backgroundColor : '#d8d'
+          backgroundColor : '#fff'
         }
       }}
     />
@@ -64,19 +81,21 @@ const renderBubble = ( props )=>{
           _id: 1,
         }}
       />
-      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+      {/* condition a custom React Native component that readjusts viewport to present input field with keyboard emerges  */}
+      { Platform.OS === 'ios' || 'android' ? <KeyboardAvoidingView behavior="height" /> : null } 
     </View>
  );
 }
 
 const styles = StyleSheet.create({
+// stretch the parent container to take up the full flex area.
  container: {
    flex: 1,
  }, 
- userSpan:{
-    fontSize: 20,
-    color: "#d88228"
- }
+//  userSpan:{
+//     fontSize: 20,
+//     color: "#d88228"
+//  }
 });
 
 export default Chat;
