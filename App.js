73b59b82d5a93d8255985426f5f4d1c9ee2bfea2
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-03-28 11:41:30 -0400

    useNetInfo establishing a boolean on state that conditions db access and passes that condition through props to Chat

--- App.js
+++ App.js
@@ -1,27 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { StyleSheet, Text, View, Alert, LogBox } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import configs from "./utils/utils";
+import {useNetInfo} from "@react-native-community/netinfo";
+
+
+
 
 const Stack = createNativeStackNavigator();
 
+
+// methods to initialize firebase and connect to firestore.
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 const App = () => {
+
+  const connectionStatus = useNetInfo();
+
+  // init app with firebase configuration object. 
   const app = initializeApp(configs);
 
+  // access the firestore database layer. 
   const db = getFirestore(app);
+
+  useEffect(()=>{
+    if(connectionStatus === false){
+      Alert.alert("Appis no longer receiving network data")
+      disableNetwork(db);
+    }else{
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
+          {/* pass db as props to the rendered Chat component of the navigation stack.  */}
+          {(props) => <Chat db={db} isConnected = {connectionStatus.isConnected} {...props} />} 
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
