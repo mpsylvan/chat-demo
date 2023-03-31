6e523a8d45280ed856668ec3b275ba0e90204ce2
mpsylvan <micperdo@gmail.com>
2023-03-31 13:56:29 -0400

    render CustomViews for React App, and ActionSheet interface

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,6 +1,14 @@
-import { StyleSheet, View, Text, KeyboardAvoidingView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Button,
+  Image,
+} from "react-native";
 import { useEffect, useState } from "react";
-import { Bubble, GiftedChat, InputToolbar} from "react-native-gifted-chat";
+import CustomActions from "./CustomActions";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import {
   collection,
   addDoc,
@@ -10,14 +18,20 @@ import {
   where,
 } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import MapView from "react-native-maps";
 
-const Chat = ({ db, isConnected, route, navigation }) => {
+const Chat = ({ db, storage, isConnected, route, navigation }) => {
   // puts a message array on state that will load in existing messages and accept new sent messages.
   const [messages, setMessages] = useState([]);
+  const [image, setImage] = useState(null);
+
+  if (image) {
+    console.log(image.uri);
+  }
 
   const { name, color, userID } = route.params;
 
-  // upon component mount update the navbar title option to be the state of user. 
+  // upon component mount update the navbar title option to be the state of user.
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
@@ -25,29 +39,27 @@ const Chat = ({ db, isConnected, route, navigation }) => {
   let unSubMessages;
 
   useEffect(() => {
-
-    if(unSubMessages) unSubMessages();
+    if (unSubMessages) unSubMessages();
     unSubMessages = null;
 
-    if(isConnected){
+    if (isConnected) {
       const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
       unSubMessages = onSnapshot(q, (docs) => {
-      let newMessages = [];
-      docs.forEach((doc) => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis()),
+        let newMessages = [];
+        docs.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
+          // call a caching function passing newMessages.
+          cacheMessages(newMessages);
+          setMessages(newMessages);
         });
-        // call a caching function passing newMessages.
-        cacheMessages(newMessages);
         setMessages(newMessages);
+        console.log(messages.length);
       });
-      setMessages(newMessages);
-      console.log(messages.length);
-    });
-    }
-    else{
+    } else {
       loadCachedMessages();
     }
     // clean up all side effects but unsubbing from firestore.
@@ -57,20 +69,22 @@ const Chat = ({ db, isConnected, route, navigation }) => {
   }, [isConnected]);
 
   // accept an array as parameter, and attempt to cache it.
-  const cacheMessages = async (messagesToCache) =>{
-    try{
-      await AsyncStorage.setItem('messages_cached', JSON.stringify(messagesToCache))
-    }
-    catch(err){
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem(
+        "messages_cached",
+        JSON.stringify(messagesToCache)
+      );
+    } catch (err) {
       console.log(err.message);
     }
-  }
-  // access 
-  const loadCachedMessages = async () =>{
-    const cachedMessages = await AsyncStorage.getItem('messages_cached') || [];
+  };
+  // access
+  const loadCachedMessages = async () => {
+    const cachedMessages =
+      (await AsyncStorage.getItem("messages_cached")) || [];
     setMessages(JSON.parse(cacheMessages));
-  }
-
+  };
 
   // resets the 'messages' state to append the newest message on every SEND executed within GiftedChat component.
   const onSend = (newMessages) => {
@@ -93,27 +107,49 @@ const Chat = ({ db, isConnected, route, navigation }) => {
       />
     );
   };
+  // callback to pass to the GiftedChat InputToolbar property. Check the state of isConnected bool, if false, don't render text input toolbar.
+  const renderInputToolbar = (props) => {
+    return isConnected ? <InputToolbar {...props} /> : null;
+  };
 
-  const renderInputToolbar = (props)=>{
-    return (isConnected) ? <InputToolbar {...props} /> : null
-    
-  }
+  const renderCustomActions = (props) => {
+    return <CustomActions userID={userID} storage={storage} {...props} />;
+  };
 
+  const renderCustomView = (props) => {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 180, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            longitudeDelta: 0.0922,
+            latitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  };
 
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
-        renderInputToolbar = {renderInputToolbar}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: userID,
           name: name,
         }}
+        renderActions={renderCustomActions}
+        renderCustomView={renderCustomView}
       />
       {/* condition a custom React Native component that readjusts viewport to present input field with keyboard emerges  */}
-      {Platform.OS === "ios" || "android" ? (
+      {Platform.OS === "ios" ? (
         <KeyboardAvoidingView behavior="height" />
       ) : null}
     </View>
